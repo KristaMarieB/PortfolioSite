eb8b424c32fad280c087e268f4dccda8e528da4f
Krista <betcherkm@gmail.com>
2019-02-28 16:04:24 -0800

    Got rid of old Blog controller/DB/etc. Creating new classes based off Blogger API Json responses.

---
+++ PortfolioSite/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioSite.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            // create httpclient
+            // intitalize properties for client
+            // using GET methods, blogger has addresses would be using
+            // make a class based off the json response
+            return View();
+        }
+    }
+}
---
+++ PortfolioSite/Models/ListPosts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortfolioSite.Models
+{
+    public class Replies
+    {
+        public string totalItems { get; set; }
+        public string selfLink { get; set; }
+    }
+
+    public class Item
+    {
+        public string kind { get; set; }
+        public string id { get; set; }
+        public Blog blog { get; set; }
+        public DateTime published { get; set; }
+        public DateTime updated { get; set; }
+        public string url { get; set; }
+        public string selfLink { get; set; }
+        public string title { get; set; }
+        public string content { get; set; }
+        public Author author { get; set; }
+        public Replies replies { get; set; }
+    }
+
+    public class BloggerPostsResponse
+    {
+        public string kind { get; set; }
+        public string nextPageToken { get; set; }
+        public string prevPageToken { get; set; }
+        public List<Item> items { get; set; }
+    }
+}
