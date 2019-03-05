8e2e962605be3f4cfaf79282d072b82174a04e39
Krista <betcherkm@gmail.com>
2019-03-05 15:45:03 -0800

    Moved around the Blog page. Took it off Home Controller and moved it to its own controller. Layout edits. Starting to attempt creating API consumption code.

--- PortfolioSite/Controllers/BlogController.cs
+++ PortfolioSite/Controllers/BlogController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,14 +10,31 @@ namespace PortfolioSite.Controllers
 {
     public class BlogController : Controller
     {
+        static HttpClient Client = new HttpClient();
+
         // GET: Blog
-        public ActionResult Index()
+        public ActionResult BlogIndex()
         {
             // create httpclient
             // intitalize properties for client
             // using GET methods, blogger has addresses would be using
             // make a class based off the json response
+
+
+
             return View();
+        }
+
+
+        public async Task<Uri> GetPostListAsync()
+        {
+            try
+            {
+                Client.BaseAddress = new Uri("https://www.googleapis.com/blogger/v3/blogs/6944967079157211687/posts");
+                HttpResponseMessage response = Client.G;
+                resposne.
+
+            }
         }
     }
 }
