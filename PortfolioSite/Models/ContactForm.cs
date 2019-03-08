b887f55cc989d78dce82518e2326155bf28322ff
Krista <37194474+KristaMarieB@users.noreply.github.com>
2019-03-08 12:00:13 -0800

    setting up contact controller

--- PortfolioSite/Models/ContactForm.cs
+++ PortfolioSite/Models/ContactForm.cs
@@ -12,11 +12,11 @@ namespace PortfolioSite.Models
         public string Name { get; set; }
 
         [Required]
-        public string LastName { get; set; }
-
-        [Required]
         [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
 
         [Required]
         public string Message { get; set; }
